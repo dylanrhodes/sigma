d56c1a163d2548b011d06e3903aca007d0d519a2
Greg Greiner <ggreiner@stanford.edu>
2014-05-02 13:05:17 -0700

    added JS to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
-'use strict';
-
-/* App Module */
+var selected = "";
+var oldColor = "";
+$(document).ready(
+  function() {  
+	$(".one-box").niceScroll({cursorcolor:"#8c8c8e", cursoropacitymin: .5});
+	$(".two-box-1").niceScroll({cursorcolor:"#8c8c8e", cursoropacitymin: .5});
+	$(".two-box-2").niceScroll({cursorcolor:"#8c8c8e", cursoropacitymin: .5});
+	
+  }
+);
+$(document).on("click", ".ind-email", function() {
+	if (selected != "") {
+		selected.css("background-color", oldColor);
+	}
+	oldColor = $(this).css("background-color");
+	$(this).css("background-color", "#e9fcfb");
+	selected = $(this);
+});
+$(document).on("click", ".ind-emailread", function() {
+	if (selected != "") {
+		selected.css("background-color", oldColor);
+	}
+	oldColor = $(this).css("background-color");
+	$(this).css("background-color", "#e9fcfb");
+	selected = $(this);
+});
