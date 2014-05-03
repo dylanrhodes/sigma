b649ee417df7507c2dd0ad0fc9444f38d773b117
Greg Greiner <ggreiner@stanford.edu>
2014-05-03 14:41:03 -0700

    Added programattic splitting
    also cut off 'from' when it extends beyond its place.

--- js/app.js
+++ js/app.js
@@ -3,9 +3,7 @@ var oldColor = "";
 $(document).ready(
   function() {  
 	$(".one-box").niceScroll({cursorcolor:"#8c8c8e", cursoropacitymin: .5, cursorwidth: 8});
-	$(".two-box-1").niceScroll({cursorcolor:"#8c8c8e", cursoropacitymin: .5, cursorwidth: 8});
-	$(".two-box-2").niceScroll({cursorcolor:"#8c8c8e", cursoropacitymin: .5, cursorwidth: 8});
-	
+	$(".two-box").niceScroll({cursorcolor:"#8c8c8e", cursoropacitymin: .5, cursorwidth: 8});
   }
 );
 $(document).on("click", ".ind-email", function() {
