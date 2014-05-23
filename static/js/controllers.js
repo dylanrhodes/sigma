a8d5dec921ae671d8c8c4601493ddf426ed0a546
Greg Greiner <ggreiner@stanford.edu>
2014-05-23 13:34:01 -0700

    scroll fix

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -421,7 +421,7 @@ sigmaApp.controller('EmailListCtrl', function($scope, $http, Emails) {
 
 					var top = temp.position().top;
 					if(top < 0) {
-						temp.parent().scrollTop(temp.parent().scrollTop() + top - 25);
+						temp.parent().scrollTop(temp.parent().scrollTop() + top - 21);
 					}
 					if ($scope.viewingEmail != null) {
 						 var target_id = temp.attr('id');
@@ -468,7 +468,7 @@ sigmaApp.controller('EmailListCtrl', function($scope, $http, Emails) {
 					var top = temp.position().top;
 					if(top >= temp.parent().height()) {
 						var dif = top - temp.parent().height();
-						temp.parent().scrollTop(temp.parent().scrollTop() + temp.height() + dif - 25);
+						temp.parent().scrollTop(temp.parent().scrollTop() + temp.height() + dif - 21);
 					}
 					if ($scope.viewingEmail != null) {
 						 var target_id = temp.attr('id');
