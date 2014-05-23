691221d10487c765516655f8492fd69c01676180
Greg Greiner <ggreiner@stanford.edu>
2014-05-23 00:20:51 -0700

    add back 3

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-var sigmaApp = angular.module('sigmaApp', ['ngAnimate', 'mgcrea.ngStrap']);
+var sigmaApp = angular.module('sigmaApp', ['ngSanitize', 'mgcrea.ngStrap']);
 
 sigmaApp.controller('EmailListCtrl', function($scope, $http, Emails) {
 
