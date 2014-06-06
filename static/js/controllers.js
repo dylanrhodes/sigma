43af5d9f69fef98c2aef2d6541903c4c994ec72a
Greg Greiner <ggreiner@stanford.edu>
2014-06-05 22:08:44 -0700

    overflow

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -501,11 +501,13 @@ sigmaApp.controller('EmailListCtrl', function($scope, $http, Emails) {
 							  if (!obj.html) {
 								$('.message-body').css('white-space', 'pre-line');
 								$('.message-body').css('padding', '0 40px');
+								$('.message-view').css('overflow', 'auto');
 								$('.message-body').html(obj.message);
 							  }
 							  else {
 								$('.message-body').css('white-space', 'normal');
 								$('.message-body').css('padding', '10px 0');
+								$('.message-view').css('overflow', 'hidden');
 								$('.message-body').html("<iframe class='email-frame' height='100%' width='100%' frameBorder='0' src='/get_email?id=" + obj.id + "' ></iframe>");
 							  }
 							  
@@ -559,11 +561,13 @@ sigmaApp.controller('EmailListCtrl', function($scope, $http, Emails) {
 							  if (!obj.html) {
 								$('.message-body').css('white-space', 'pre-line');
 								$('.message-body').css('padding', '0 40px');
+								$('.message-view').css('overflow', 'auto');
 								$('.message-body').html(obj.message);
 							  }
 							  else {
 								$('.message-body').css('white-space', 'normal');
 								$('.message-body').css('padding', '10px 0');
+								$('.message-view').css('overflow', 'hidden');
 								$('.message-body').html("<iframe class='email-frame' height='100%' width='100%' frameBorder='0' src='/get_email?id=" + obj.id + "' ></iframe>");
 							  }
 							}
@@ -611,11 +615,13 @@ sigmaApp.controller('EmailListCtrl', function($scope, $http, Emails) {
 			  if (!obj.html) {
 				$('.message-body').css('white-space', 'pre-line');
 				$('.message-body').css('padding', '0 40px');
+				$('.message-view').css('overflow', 'auto');
 				$('.message-body').html(obj.message);
 			  }
 			  else {
 				$('.message-body').css('white-space', 'normal');
 				$('.message-body').css('padding', '10px 0');
+				$('.message-view').css('overflow', 'hidden');
 				$('.message-body').html("<iframe class='email-frame' height='100%' width='100%' frameBorder='0' src='/get_email?id=" + obj.id + "' ></iframe>");
 			  }
 			}
@@ -656,11 +662,13 @@ sigmaApp.controller('EmailListCtrl', function($scope, $http, Emails) {
 					  if (!obj.html) {
 						$('.message-body').css('white-space', 'pre-line');
 						$('.message-body').css('padding', '0 40px');
+						$('.message-view').css('overflow', 'auto');
 						$('.message-body').html(obj.message);
 					  }
 					  else {
 						$('.message-body').css('white-space', 'normal');
 						$('.message-body').css('padding', '10px 0');
+						$('.message-view').css('overflow', 'hidden');
 						$('.message-body').html("<iframe class='email-frame' height='100%' width='100%' frameBorder='0' src='/get_email?id=" + obj.id + "' ></iframe>");
 					  }
 					}
