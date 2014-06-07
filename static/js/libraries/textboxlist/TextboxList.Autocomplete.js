5113b836f7f9adf4662661bc1d7a032ab97c1760
Luke Knepper <luke@knepper.com>
2014-06-06 17:03:40 -0700

    tblololol

--- static/js/libraries/textboxlist/TextboxList.Autocomplete.js
+++ static/js/libraries/textboxlist/TextboxList.Autocomplete.js
@@ -39,7 +39,7 @@ $.TextboxList.Autocomplete = function(textboxlist, _options){
 			.addEvent('bitEditableFocus', search)
 			.addEvent('bitEditableBlur', hide)
 			.setOptions({bitsOptions: {editable: {addKeys: false, stopEnter: false}}});
-		if ($.browser.msie) textboxlist.setOptions({bitsOptions: {editable: {addOnBlur: false}}});
+		if ($.browser && $.browser.msie) textboxlist.setOptions({bitsOptions: {editable: {addOnBlur: false}}});
 		prefix = textboxlist.getOptions().prefix + '-autocomplete';
 		method = $.TextboxList.Autocomplete.Methods[options.method];
 		container = $('<div class="'+ prefix +'" />').width(textboxlist.getContainer().width()).appendTo(textboxlist.getContainer());
@@ -127,7 +127,7 @@ $.TextboxList.Autocomplete = function(textboxlist, _options){
 			hidePlaceholder();
 			list.css('display', 'none');
 			currentSearch = null;			
-		}, $.browser.msie ? 150 : 0);
+		}, ($.browser && $.browser.msie) ? 150 : 0);
 	};
 	
 	var showPlaceholder = function(){
